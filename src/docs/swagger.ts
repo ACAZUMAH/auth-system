c5e2e18962ac737cc37b555665d6041ea13dc939
ACAZUMAH <calebazumah9@gmail.com>
2024-10-19 00:50:50 +0000

    docs

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -17,10 +17,18 @@ const swaggerOptions = {
     apis: ['dist/routers/*.js']
 }
 
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
+
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
 
 const setUpSwagger = (app: any) => {
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs,
+        {
+            customCss:
+            '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+            customCssUrl: CSS_URL,
+        }
+    ));
 };
 
 export default setUpSwagger;
